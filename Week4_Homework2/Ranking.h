57d04de9a3a5cdf84923ffcf4156c7bf998c9863
JYPark0803 <103144581+JYPark0803@users.noreply.github.com>
2022-05-07 14:39:48 +0900

    Create Ranking.h

---
+++ Week4_Homework2/Ranking.h
@@ -0,0 +1,14 @@
+#pragma warning (disable:4996)
+#pragma once
+#include<cstdio>
+
+struct PlayInfo
+{
+    char name[200];
+    int nMove;
+    double tElapsed;
+};
+extern void loadRanking(const char* filename);
+extern void storeRanking(const char* filename);
+extern void printRanking();
+extern int addRanking(int nMove, double tElap);
